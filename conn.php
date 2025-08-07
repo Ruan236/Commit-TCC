2e9ece0d0f1ddfeb03529bcb0f58c7df1c747f28
Otaviocuriel <otavio.curiel@etec.sp.gov.br>
2025-08-07 11:02:02 -0300

    Limpeza

--- conn.php
+++ conn.php
@@ -1,13 +1,13 @@
 <?php
-$servidor = "localhost"; // Ou IP do servidor MySQL
-$usuario = "root"; // Usuário do MySQL
-$senha = ""; // Senha do MySQL (deixe vazia se estiver local)
-$banco = "teste_tcc"; // Nome do banco de dados
+$servidor = "localhost"; 
+$usuario = "root"; 
+$senha = ""; 
+$banco = "teste_tcc"; 
 
-// Criando a conexão
+
 $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
-// Verificando a conexão
+
 if ($conexao->connect_error) {
     die("Falha na conexão: " . $conexao->connect_error);
 } else {
